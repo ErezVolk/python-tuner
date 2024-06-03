e04dc7a4a420ab9a5fb7a9d331c50697c44a5e86
Erez Volk <erez@neuralight.ai>
2024-06-03 10:50:28 +0300

    Lint

--- tuner.py
+++ tuner.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""A minimal command-line guitar/ukulele tuner in Python."""
 ######################################################################
 # tuner.py - a minimal command-line guitar/ukulele tuner in Python.
 # Requires numpy and pyaudio.
@@ -43,14 +44,17 @@ NOTE_NAMES = "C C# D D# E F F# G G# A A# B".split()
 
 
 def freq_to_number(f):
+    """Convert frequency (Hz) to MIDI number."""
     return 69 + 12 * np.log2(f / 440.0)
 
 
 def number_to_freq(n):
+    """Convert MIDI number to frequency (Hz)."""
     return 440 * 2.0 ** ((n - 69) / 12.0)
 
 
 def note_name(n):
+    """Give name ('C#', etc.) of MIDI note."""
     return NOTE_NAMES[n % 12] + str(n / 12 - 1)
 
 
@@ -61,56 +65,64 @@ def note_name(n):
 # Get min/max index within FFT of notes we care about.
 # See docs for numpy.rfftfreq()
 def note_to_fftbin(n):
+    """Convert MIDI number to FFT bin(?)."""
     return number_to_freq(n) / FREQ_STEP
 
 
-imin = max(0, int(np.floor(note_to_fftbin(NOTE_MIN - 1))))
-imax = min(SAMPLES_PER_FFT, int(np.ceil(note_to_fftbin(NOTE_MAX + 1))))
+def main():
+    """Tune a guitar."""
+    imin = max(0, int(np.floor(note_to_fftbin(NOTE_MIN - 1))))
+    imax = min(SAMPLES_PER_FFT, int(np.ceil(note_to_fftbin(NOTE_MAX + 1))))
 
-# Allocate space to run an FFT.
-buf = np.zeros(SAMPLES_PER_FFT, dtype=np.float32)
-num_frames = 0
+    # Allocate space to run an FFT.
+    buf = np.zeros(SAMPLES_PER_FFT, dtype=np.float32)
+    num_frames = 0
 
-# Initialize audio
-stream = pyaudio.PyAudio().open(
-    format=pyaudio.paInt16,
-    channels=1,
-    rate=FSAMP,
-    input=True,
-    frames_per_buffer=FRAME_SIZE,
-)
+    # Initialize audio
+    stream = pyaudio.PyAudio().open(
+        format=pyaudio.paInt16,
+        channels=1,
+        rate=FSAMP,
+        input=True,
+        frames_per_buffer=FRAME_SIZE,
+    )
 
-stream.start_stream()
+    stream.start_stream()
 
-# Create Hanning window function
-window = 0.5 * (1 - np.cos(np.linspace(0, 2 * np.pi, SAMPLES_PER_FFT, False)))
+    # Create Hanning window function
+    window = 0.5 * (
+        1 - np.cos(np.linspace(0, 2 * np.pi, SAMPLES_PER_FFT, False))
+    )
 
-# Print initial text
-print("sampling at", FSAMP, "Hz with max resolution of", FREQ_STEP, "Hz")
-print()
+    # Print initial text
+    print("sampling at", FSAMP, "Hz with max resolution of", FREQ_STEP, "Hz")
+    print()
 
-# As long as we are getting data:
-while stream.is_active():
-    # Shift the buffer down and new data in
-    buf[:-FRAME_SIZE] = buf[FRAME_SIZE:]
-    buf[-FRAME_SIZE:] = np.fromstring(stream.read(FRAME_SIZE), np.int16)
+    # As long as we are getting data:
+    while stream.is_active():
+        # Shift the buffer down and new data in
+        buf[:-FRAME_SIZE] = buf[FRAME_SIZE:]
+        buf[-FRAME_SIZE:] = np.fromstring(stream.read(FRAME_SIZE), np.int16)
 
-    # Run the FFT on the windowed buffer
-    fft = np.fft.rfft(buf * window)
+        # Run the FFT on the windowed buffer
+        fft = np.fft.rfft(buf * window)
 
-    # Get frequency of maximum response in range
-    freq = (np.abs(fft[imin:imax]).argmax() + imin) * FREQ_STEP
+        # Get frequency of maximum response in range
+        freq = (np.abs(fft[imin:imax]).argmax() + imin) * FREQ_STEP
 
-    # Get note number and nearest note
-    n = freq_to_number(freq)
-    n0 = int(round(n))
+        # Get note number and nearest note
+        n = freq_to_number(freq)
+        n0 = int(round(n))
 
-    # Console output once we have a full buffer
-    num_frames += 1
+        # Console output once we have a full buffer
+        num_frames += 1
 
-    if num_frames >= FRAMES_PER_FFT:
-        print(
-            "freq: {:7.2f} Hz     note: {:>3s} {:+.2f}".format(
-                freq, note_name(n0), n - n0
+        if num_frames >= FRAMES_PER_FFT:
+            print(
+                f"freq: {freq:7.2f} Hz     "
+                f"note: {note_name(n0):>3s} {n - n0:+.2f}"
             )
-        )
+
+
+if __name__ == "__main__":
+    main()
